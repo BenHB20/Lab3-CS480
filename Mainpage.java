106b09a590964b93aa5e5b6a190de6f99c242e99
BenHB20 <109940771+BenHB20@users.noreply.github.com>
2025-01-22 15:23:21 -0800

    basic GUI finished with general outline
    Created the GUI with all required operations.

---
+++ Mainpage.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+
+public class Mainpage {
+    private JTextPane calculationPane;
+    private JPanel MainPanel;
+    private JPanel mainButtonPanel;
+    private JButton eraseButton;
+    private JButton zeroButton;
+    private JButton enterButton;
+    private JButton oneButton;
+    private JButton twoButton;
+    private JButton threeButton;
+    private JButton fiveButton;
+    private JButton sixButton;
+    private JButton fourButton;
+    private JButton nineButton;
+    private JButton eightButton;
+    private JButton sevenButton;
+    private JPanel signPanel;
+    private JButton addButton;
+    private JButton divButton;
+    private JButton multButton;
+    private JButton subButton;
+    private JButton exponentButton;
+    private JButton lnButton;
+    private JButton log10Button;
+    private JButton parLeftButton;
+    private JButton cotButton;
+    private JButton tanButton;
+    private JButton cosButton;
+    private JButton sinButton;
+    private JButton parRightButton;
+    private JButton rightCurlButton;
+    private JButton leftCurlButton;
+    private JButton clearButton;
+    private JPanel modifierPanel;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
